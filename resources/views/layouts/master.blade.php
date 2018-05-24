be3f7a6de8b90c644d7fe6d8eff9fdad25bf4916
JP Roux <jprouxglobal@gmail.com>
2018-05-24 06:38:50 +0200

    Including master blade template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tom_CRUD - @yield('title')</title>
+    </head>
+    <body>
+    <p>[HEADER]</p>
+
+    @yield('content')
+
+    <p>[FOOTER]</p>
+    </body>
+</html>
--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>{{$member->name}}</title>
-    </head>
-    <body>
-        <h1>Herewith Member Details: </h1>
-        <p>Name: {{$member->name}}</p>
-        <p>Surname: {{$member->surname}}</p>
-        <p>ID Nr: {{$member->id_number}}</p>
-        <p>Mobile Nr: {{$member->mobile_number}}</p>
-        <p>Email: {{$member->email}}</p>
-        <p>D.O.B: {{$member->date_of_birth}}</p>
-        <p>Created At: {{$member->created_at}}</p>
-    </body>
-</html>
+@extends('layouts.master')
+
+@section('title', $member->name)
+
+@section('content')
+<h1>Herewith Member Details: </h1>
+<p>Name: {{$member->name}}</p>
+<p>Surname: {{$member->surname}}</p>
+<p>ID Nr: {{$member->id_number}}</p>
+<p>Mobile Nr: {{$member->mobile_number}}</p>
+<p>Email: {{$member->email}}</p>
+<p>D.O.B: {{$member->date_of_birth}}</p>
+<p>Created At: {{$member->created_at}}</p>
+@endsection
